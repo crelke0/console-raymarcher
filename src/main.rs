802b483a028dc555956a1a78293aed3be02cf4ec
Clarke Elke <clarkeelke@Sunshines-MBP.attlocal.net>
2023-04-28 13:37:46 -0500

    fixed demos to work with changes in raymarch.rs and render.rs. added a demo (demo2) that has the tree different types of objects displayed at once

--- src/main.rs
+++ src/main.rs
@@ -10,11 +10,14 @@ use crate::render::render::{Camera, render};
 const SMALLEST_STEP: f32 = 0.05;
 const MAX_STEPS: i32 = 20;
 
-
 fn demo0() {
     let mut theta = 0f32;
     let mut counter = 0;
-    let objects = [Sphere::new(Vec3D::new(-0.5, 0.0, 0.0), 2.5), Sphere::new(Vec3D::new(4.7, 1.2, 0.5), 2.0), Sphere::new(Vec3D::new(-3.7, -3.2, -2.5), 1.5)];
+    let objects: [&dyn Raymarchable; 3] = [
+      &Sphere::new(Vec3D::new(-0.5, 0.0, 0.0), 2.5),
+      &Sphere::new(Vec3D::new(4.7, 1.2, 0.5), 2.0),
+      &Sphere::new(Vec3D::new(-3.7, -3.2, -2.5), 1.5)
+    ];
     let light = Vec3D::new(3.0, 3.0, -2.0);
     let mut camera = Camera::new(Vec3D::new(0.0, 0.0, 0.0), Vec3D::new(0.0, 1.0, 0.0), 1.0);
     loop {
@@ -31,7 +34,7 @@ fn demo0() {
     }
   }
 fn demo1() {
-  let mut objects = [Torus::new(Vec3D::new(0.0, 0.0, 0.0), 3.0, 1.0)];
+  let objects: [&dyn Raymarchable; 1] = [&Torus::new(Vec3D::new(0.0, 0.0, 0.0), 3.0, 1.0)];
   let light = Vec3D::new(2.0, 1.0, -2.0);
   let mut camera = Camera::new(Vec3D::new(0.0, 0.0, 0.0), Vec3D::new(0.0, 1.0, 0.0), 1.0);
   let mut theta = 0f32;
@@ -42,7 +45,30 @@ fn demo1() {
       Some(x) => x,
       None => {panic!();}
     };
-    objects[0].pos.z = (theta*1.0).sin()*1.2;
+    render(&objects, &camera, &light, 100, 70, 1, 0.4, MAX_STEPS, SMALLEST_STEP);
+    counter += 1;
+    theta += 0.1;
+    theta %= TAU;
+    if counter > 200 { break; }
+  }
+}
+
+fn demo2() {
+  let objects: [&dyn Raymarchable; 3] = [
+    &Cuboid::new(Vec3D::new(0.0, 0.0, 0.0), Vec3D::new(2.0, 3.0, 1.0)),
+    &Torus::new(Vec3D::new(4.0, 1.0, 0.0), 1.0, 0.5),
+    &Sphere::new(Vec3D::new(-4.0, 0.0, 0.0), 2.0)
+  ];
+  let light = Vec3D::new(-3.0, -3.0, -4.0);
+  let mut camera = Camera::new(Vec3D::new(0.0, 0.0, 0.0), Vec3D::new(0.0, 1.0, 0.0), 1.0);
+  let mut counter = 0;
+  let mut theta = 0f32;
+  loop {
+    camera.pos = Vec3D::new(theta.cos()*20.0, theta.sin()*20.0, -10.0);
+    camera.dir = match (Vec3D::new(0.0,0.0,2.0)-camera.pos).normalize() {
+      Some(x) => x,
+      None => {panic!();}
+    };
     render(&objects, &camera, &light, 100, 70, 1, 0.4, MAX_STEPS, SMALLEST_STEP);
     counter += 1;
     theta += 0.1;
@@ -52,7 +78,7 @@ fn demo1() {
 }
 
 fn main() {
-  let demos = [demo0, demo1];
+  let demos = [demo0, demo1, demo2];
   loop {
     println!("Enter a number from 0 to {} to select a demo, or type \"q\" to quit: ", demos.len() - 1);
     let mut input = String::new();
